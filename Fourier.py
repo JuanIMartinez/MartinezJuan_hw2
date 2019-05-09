befe20f565b0ad722ca59e752ad8a2f1009614d1
JuanIMartinez <42219545+JuanIMartinez@users.noreply.github.com>
2019-05-08 23:15:22 -0500

    Fourier

--- Fourier.py
+++ Fourier.py
@@ -1,4 +1,4 @@
-# Ejercicio 2: Transformada de fourier 
+﻿# Ejercicio 2: Transformada de fourier 
 
 import numpy as np
 import matplotlib.pylab as plt
@@ -12,7 +12,7 @@ dt=(tiempo[-1]-tiempo[0])/len(tiempo)
 
 
 signal=np.genfromtxt("signal.dat",usecols=(1))
-len_sigma=len(signal)
+
 
 
 ##signalSuma.dat
@@ -21,7 +21,7 @@ tiempo_suma=np.genfromtxt("signalSuma.dat",usecols=(0))
 dt_suma=(tiempo_suma[-1]-tiempo_suma[0])/len(tiempo_suma)
 
 signal_suma=np.genfromtxt("signalSuma.dat",usecols=(1))
-len_sigmaSuma=len(signal_suma)
+
 
 
 ## se grafican las señales de signal.dat y signalSuma.dat.
@@ -47,30 +47,31 @@ plt.close()
 ##implementacion de la transformada discreta de Fourier.
 
 def transformada_fourier(data):
-    fourier=[]
+    fourier=np.empty(0)
     for i in range(len(data)):
         factor=0
         for j in range(len(data)):
-               factor=factor+(np.cos(-2*np.pi*j/len(data))+1j*np.sin(-2*np.pi*j/len(data)))*data[i]
-        fourier.append(factor)
-        
+            factor=factor+data[j]*np.exp(-2*np.pi/len(data)*j*i*1j)
+            incluir=abs(factor)/len(data)
+        fourier=np.append(fourier,incluir)
+    return fourier
+
+       
 ## Transformada de Fourier de signal y signalSuma con implementacion Propia
 
-fourier_signal_propia=transformada_fourier(signal)
-fourier_signalSuma_propia=transformada_fourier(signal_suma)
+fourier_signal=transformada_fourier(signal)
+fourier_signalSuma=transformada_fourier(signal_suma)
 
 ##Transformada de Fourier de signal y signalSuma 
 
-fourier_signal = np.fft.fft(signal)
+
 len_signal=len(signal)
-fftfreq_signal=np.fft.fftfreq(len_signal,dt)
-fftfreq_signal_plot=np.abs(fftfreq_signal)
+fftfreq_signal_plot=dt/len(signal)*np.linspace(0,len(signal),len(signal)) ##IMPLEMENTACION QUE REEMPLAZA FFTFREQ
 
 
-fourier_signalSuma = np.fft.fft(signal_suma)
+
 len_signalSuma=len(signal_suma)
-fftfreq_signalSuma=np.fft.fftfreq(len_signalSuma,dt_suma)
-fftfreq_signalSuma_plot=np.abs(fftfreq_signalSuma)
+fftfreq_signalSuma_plot=dt/len(signal_suma)*np.linspace(0,len(signal_suma),len(signal_suma)) ##IMPLEMENTACION QUE REEMPLAZA FFTFREQ
 
 
 
@@ -103,3 +104,8 @@ plt.specgram(signal, NFFT=256*2, Fs=0.3)
 plt.grid()
 plt.savefig("Espectograma.pdf")
 plt.close()
+
+#####temblor
+
+signal=np.genfromtxt("signal.dat",usecols=(1))
+len_sigma=len(signal)
