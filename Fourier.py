3831d923206620c415c54d4ff0bf368222ccddc3
JuanIMartinez <42219545+JuanIMartinez@users.noreply.github.com>
2019-05-09 05:11:00 -0500

    Fourier

--- Fourier.py
+++ Fourier.py
@@ -1,4 +1,4 @@
-﻿# Ejercicio 2: Transformada de fourier 
+# Ejercicio 2: Transformada de fourier 
 
 import numpy as np
 import matplotlib.pylab as plt
@@ -30,12 +30,12 @@ signal_suma=np.genfromtxt("signalSuma.dat",usecols=(1))
 plt.figure()
 plt.subplot(2,1,1)
 plt.title('SignalSuma y Signal')
-plt.plot(tiempo_suma,signal_suma,c="lightsalmon",label = "SignalSuma")
+plt.plot(tiempo_suma,signal_suma,c="lightsalmon",label = "SignalSuma", linewidth=0.7)
 plt.ylabel("SignalSuma")
 plt.grid()
 plt.legend()
 plt.subplot(2,1,2)
-plt.plot(tiempo,signal,c="c",label = "Signal")
+plt.plot(tiempo,signal,c="c",label = "Signal", linewidth=0.7)
 plt.ylabel("Signal")
 plt.xlabel("Tiempo")
 plt.grid()
@@ -78,12 +78,12 @@ fftfreq_signalSuma_plot=dt/len(signal_suma)*np.linspace(0,len(signal_suma),len(s
 plt.figure()
 plt.subplot(2,1,1)
 plt.title('Transformada Fourier de SignalSuma y Signal')
-plt.plot(fftfreq_signalSuma_plot,fourier_signalSuma,c="skyblue",label = "T. SignalSuma")
+plt.plot(fftfreq_signalSuma_plot,fourier_signalSuma,c="skyblue",label = "T. SignalSuma", linewidth=0.7)
 plt.ylabel("T. SignalSuma")
 plt.grid()
 plt.legend()
 plt.subplot(2,1,2)
-plt.plot(fftfreq_signal_plot,fourier_signal,c="tomato",label = "T. Signal")
+plt.plot(fftfreq_signal_plot,fourier_signal,c="tomato",label = "T. Signal", linewidth=0.7)
 plt.ylabel("T. Signal")
 plt.xlabel("Frecuencia")
 plt.grid()
@@ -95,17 +95,61 @@ plt.close()
 ##espectogramas
 
 plt.figure()
-plt.subplot(1,2,1)
+plt.subplot(2,1,1)
 plt.title('Transformada Fourier de SignalSuma y Signal')
-plt.specgram(signal_suma, NFFT=256*2, Fs=0.3)
+plt.specgram(signal_suma, NFFT=256*2, Fs=dt_suma)
+plt.colorbar()
 plt.grid()
-plt.subplot(1,2,2)
-plt.specgram(signal, NFFT=256*2, Fs=0.3)
+plt.subplot(2,1,2)
+plt.specgram(signal, NFFT=256*2, Fs=dt)
+plt.colorbar()
 plt.grid()
 plt.savefig("Espectograma.pdf")
 plt.close()
 
-#####temblor
+print("SE UTILIZO UNA IMPLEMENTACION PROPIA PARA REEMPLAZAR FFTFREQ")
 
-signal=np.genfromtxt("signal.dat",usecols=(1))
-len_sigma=len(signal)
+
+#####se almacena temblor.txt y se grafica
+
+temblor=np.genfromtxt("temblor.txt")[4:]
+dt_temblor=1/100
+tiempo_temblor=np.linspace(0,len(temblor)/dt,len(temblor))
+
+
+plt.figure()
+plt.plot(tiempo_temblor,temblor, c="springgreen", label="signal Temblor", linewidth=0.7)
+plt.xlabel("tiempo [s]")
+plt.ylabel("signal")
+plt.title("Signal Temblor")
+plt.legend()
+plt.grid()
+plt.savefig("SignalTemblor.pdf")
+plt.close()
+
+## transformada 
+
+fourier_temblor = abs( np.fft.fft(temblor))
+cantidad_datos_temblor=len(temblor)
+fftfreq=np.fft.fftfreq(cantidad_datos_temblor,dt_temblor)
+fftfreq_plot_temblor=abs(fftfreq)
+
+plt.figure()
+plt.plot(fftfreq_plot_temblor,fourier_temblor, c="orange", label="T. signal Temblor", linewidth=0.7)
+plt.xlabel("frecuencia (1/s)")
+plt.ylabel("T. Signal Temblor")
+plt.title("Transformada de Fourier Signal Temblor")
+plt.grid()
+plt.legend()
+plt.savefig("Fourier_temblor.pdf")
+plt.close()
+
+##espectrograma de Temblor.txt
+
+plt.figure()
+plt.title('Transformada Fourier de SignalSuma y Signal')
+plt.specgram(temblor, NFFT=256, Fs=dt_temblor)
+plt.colorbar()
+plt.grid()
+plt.savefig("Espectograma_temblor.pdf")
+plt.close()
